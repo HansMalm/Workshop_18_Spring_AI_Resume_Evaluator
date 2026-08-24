d5cb0371982a5a96642614b5d45632019873c958
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-08-24 22:48:54 +0200

    initial commit

---
+++ src/main/java/lexicon/hans/workshop_18_spring_ai_resume_evaluator/Workshop18SpringAiResumeEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package lexicon.hans.workshop_18_spring_ai_resume_evaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop18SpringAiResumeEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workshop18SpringAiResumeEvaluatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lexicon/hans/workshop_18_spring_ai_resume_evaluator/Workshop18SpringAiResumeEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package lexicon.hans.workshop_18_spring_ai_resume_evaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop18SpringAiResumeEvaluatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
